7f7ee07683f189ac6d11df53b4ae3a57967eab05
Zhang Yi <zy1581068@gmail.com>
2025-08-30 05:41:46 +0800

    classification hw

--- coco/dataset/classification_dataset.py
+++ coco/dataset/classification_dataset.py
@@ -4,4 +4,48 @@ from torch.utils.data.dataset import Dataset
 from torch.utils.data.dataloader import DataLoader
 
 
-class ClassificationDataset(Dataset):
+class CustomImageDataset(Dataset):
+    def __init__(self, data_dir, mode="train"):
+        super().__init__()
+        """
+        Data format
+        data_dir
+           |
+           |----annotations
+           |
+           |----train2017
+           |
+           |----val2017
+        """
+        # get annotation file path
+        if mode == "train":
+            self.image_dir = os.path.join(data_dir, "train2017")
+            annotation_file = os.path.join(data_dir, "annotations", "person_keypoints_train2017.json")
+        elif mode == "test":
+            self.image_dir = os.path.join(data_dir, "val2017")
+            annotation_file = os.path.join(data_dir, "annotations", "person_keypoints_val2017.json")
+        else:
+            raise ValueError(f"Unknown mode: {mode}")
+
+        # load annotations
+        with open(annotation_file, "r") as f:
+            self.annotations = json.load(f)
+
+        # load id to image name mapping
+        self.ids_to_image_paths = {e['id']: e['file_name'] for e in self.annotations["images"]}
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self, idx):
+        class_ID = self.annotations["annotations"][idx]["category_id"]
+        image_path = os.path.join(self.image_dir, self.ids_to_image_paths[self.annotations["annotations"][idx]["image_id"]])
+        image = Image.open(image_path)
+        return image, class_ID
+
+
+if __name__ == "__main__":
+    dataset = CustomImageDataset(data_dir="C:/Users/22969/OneDrive/文档/application/大语言模型科研/data", mode="test")
+    image, class_id = dataset[0]
+    print(f"image: {image}")
+    print(f"class_id: {class_id}")
